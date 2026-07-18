a097eebdb20f6c90cf3f913cc70f5ead05291db1
rndchristi-cell <rendychristi@gmail.com>
2026-07-19 06:32:23 +0700

    Remove Tailwind integration and markdown settings
    Removed Tailwind integration and markdown configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,6 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind()],
   output: 'static',
   site: 'https://ecclesiola.com',
-  markdown: {
-    shikiConfig: {
-      theme: 'github-dark',
-    },
-  },
 });
